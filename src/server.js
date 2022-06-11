8a68894f95e5de5bb5f72a9728410c5887901037
painkiller <commitment1511@outlook.com>
2022-06-11 22:24:48 +0700

    404 server.js

--- src/server.js
+++ src/server.js
@@ -1,42 +1,42 @@
-require("dotenv").config()
-const express = require('express')
-const bodyParser = require('body-parser')
-const cookieParser = require('cookie-parser')
+require("dotenv").config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
 // const { connection } = require('./config/connectDB')
 
-const configViewEngine = require("./config/viewEngine")
-const initWebRoutes = require("./routes/web")
-const initApiRoutes = require('./routes/api')
-const { configCors } = require('./config/cors')
+const configViewEngine = require("./config/viewEngine");
+const initWebRoutes = require("./routes/web");
+const initApiRoutes = require("./routes/api");
+const { configCors } = require("./config/cors");
 
-const app = express()
-const hostname = 'localhost';
-const PORT = process.env.PORT || 8080
+const app = express();
+const hostname = "localhost";
+const PORT = process.env.PORT || 8080;
 
 //config cors
-configCors(app)
+configCors(app);
 
 //config view engine
-configViewEngine(app)
+configViewEngine(app);
 
 //body-parser
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 //config cookie parser
-app.use(cookieParser())
+app.use(cookieParser());
 
 //test connection
 // connection()
 
 //init web routes
-initWebRoutes(app)
-initApiRoutes(app)
+initWebRoutes(app);
+initApiRoutes(app);
 
 app.use((req, res) => {
-    return res.send('404 not found')
-})
+    return res.status(404).json({ EM: "404 not found" });
+});
 
 app.listen(PORT, () => {
-    console.log(">>> JWT Backend is running on the port = " + PORT)
-})
+    console.log(">>> JWT Backend is running on the port = " + PORT);
+});
